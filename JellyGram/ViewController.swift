0f9b132cce979e7240be655c3e5e7563ab4b9fe7
jelly <chm073@gmail.com>
2019-06-15 23:19:01 +0900

    Initial Commit

---
+++ JellyGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JellyGram
+//
+//  Created by jelly on 15/06/2019.
+//  Copyright © 2019 jellyms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
